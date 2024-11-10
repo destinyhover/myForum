8e99a04296814515029ca1aa7b4910c7df1eaca0
Your Name <you@example.com>
2024-11-10 15:02:12 +0300

    Second

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/' do
 	erb "Hello! </a>"			
+end
+
+get '/lasttopics' do
+  "Hello World"
+end
+
+get '/newpost' do
+  erb :newpost
 end
