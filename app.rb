70e7017d2dc6800f8f35c230794f9bf55d44da70
Your Name <you@example.com>
2024-11-10 17:39:13 +0300

    Fix all stuff

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ configure do
 	$db = SQLite3::Database.new 'forumdb.db'
 	$db.execute'create table if not exists "Content" (id Integer, content Text, time Date, Primary Key("id" Autoincrement));'
 	$db.execute'create table if not exists "Users" (id Integer, Username Text, post_id Integer, Primary Key("id" Autoincrement));'
-$db.results_as_hash = true
+	$db.execute'create table if not exists "Comments" (id Integer, content Text, post_id Integer, created_date Date, Primary Key("id" Autoincrement));'
+	$db.results_as_hash = true
 
 	# $db.close
 end
@@ -18,13 +19,14 @@ end
 #   return @db
 # end
 
-get '/' do
+get '/lasttopics' do
 		@results = $db.execute 'select * from Content order by id desc'	
-	erb :index			
+	erb :lasttopics			
 end
 
-get '/lasttopics' do
-  "Hello World"
+get '/' do
+  erb "<h1>My simple forum here</h1>"
+
 end
 
 get '/newpost' do
@@ -35,7 +37,7 @@ post '/newpost' do
 	# получаем переменную из post-запроса
 	@username =params[:username]
 	@content = params[:content]
-	if @content.strip.length <= 5 && @username.length <= 1
+	if @content.strip.length <= 5 or @username.length <= 1
 		@error = 'Enter right'
 		return erb :newpost
 	end
@@ -44,7 +46,7 @@ post '/newpost' do
 
 	$db.execute 'insert into Content (content, time) values (?,datetime())', [@content]
 	post_id = $db.last_insert_row_id
-	$db.execute 'insert into Users (Username, post_id) values (?,?)', [@content, post_id]
+	$db.execute 'insert into Users (Username, post_id) values (?,?)', [@username, post_id]
 
 	# перенаправление на главную страницу
 
@@ -52,3 +54,28 @@ post '/newpost' do
 end
 
 
+get '/details/:post_id' do
+
+	# получаем переменную из url'a
+	post_id = params[:post_id]
+
+	# получаем список постов
+	# (у нас будет только один пост)
+	results = $db.execute 'select * from Content where id = ?', [post_id]
+	name1 = $db.execute 'select Username from Users where post_id = ?', [post_id]		
+	@name = name1[0]
+	# выбираем этот один пост в переменную @row
+	@row = results[0]
+
+	# выбираем комментарии для нашего поста
+	@comments = $db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+
+	# возвращаем представление details.erb
+	erb :details
+end
+post '/details/:post_id' do
+	post_id=params[:post_id]
+	content=params[:content]
+	$db.execute 'insert into Comments (content, post_id, created_date) values (?,?, datetime())', [content, post_id]
+	redirect to ('/details/' + post_id)
+end
